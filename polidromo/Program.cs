6213c8f53fb3c704720ba31c56a918b9001bb04b
jesusdanielgascaalvarado <jesusdanielgascaalvarado@gmail.com>
2026-02-18 17:12:57 -0600

    Add project files.

---
+++ polidromo/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+polindromos.isPolindromos polidromo = new polindromos.isPolindromos();
+
+int a = 0;
+bool validation = false;
+try
+{
+    a = (int)Convert.ToDouble(Console.ReadLine());
+
+    validation = polidromo.checker(a);
+
+    if (validation == false)
+    {
+        Console.WriteLine("No es Polindromos");
+    }
+    else
+    {
+        Console.WriteLine("Es Polindromos");
+    }
+}catch
+{
+    Console.WriteLine("No es numerico"); 
+}
+
+
+
---
+++ polidromo/isPolidromo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace polindromos
+{
+    internal class isPolindromos
+    {
+        public bool checker(int a)
+        {
+            if (a <= 0 ) return false;
+            
+            int digitoL = (int)Math.Floor(Math.Log10(a) + 1);
+            if (digitoL == 1) return true;
+            //int[] digitos = new int[(int)Math.Floor(Math.Log(a) + 1)];
+            int[] digitos = new int[digitoL];
+
+
+            int temp = a;
+
+            for (int i = digitos.Length - 1; i >= 0; i--)
+            {
+                digitos[i] = temp % 10;
+                temp /= 10;
+            }
+            long result = 0;
+            long multiplier = 1;
+
+     
+            for (int i = 0; i <= digitos.Length - 1; i++)
+            {
+                result += digitos[i] * multiplier;
+                multiplier *= 10;
+
+            }
+
+            if (result == a)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+}
